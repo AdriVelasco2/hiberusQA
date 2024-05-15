8ed0ae4a18318eaf8901acd193f6f6add2299ed4
AdriVelasco2 <isadri35@hotmail.com>
2024-05-15 19:37:07 +0200

    primer commit

---
+++ src/main/java/com/hiberus/university/adrian/velasco/first/App.java
@@ -0,0 +1,13 @@
+package com.hiberus.university.adrian.velasco.first;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
